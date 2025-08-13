5117b451eddb231aa197eee94b3950a884741d9f
Hogan McCune <hm127038@student.wpsok.org>
2025-08-13 13:01:21 -0500

    Added HW1.

---
+++ HW2.py
@@ -0,0 +1,34 @@
+#Name: Hogan McCune
+#Class: 5th Hour
+#Assignment: HW2
+
+import math
+#1. Print "Hello World!"
+print ("Hello World!")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+test = 1200000000
+Test2 = "TenBillionRabbits"
+Test3 = True
+Test4 = False
+Space = " "
+#3. Print all three variables on the same print function (at the same time).
+print (test, Space, Test2, Space, Test3, Space, Test4)
+#4. Create a variable that asks the user to input an integer.
+X = int(input("Enter a number: "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+z = test + X
+print (z)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print (z/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+Test3 = False
+Test4 = True
+#8. Print the value of the boolean variable.
+print (Test3, Test4)
+#9. Create a variable with a number that contains decimals.
+variable = 10.1
+#10. Round the number from #9 up or down using the round function.
+print (" ")
+print (variable)
+print ("rounded is")
+print (round(variable))
---
+++ Testing for Fun.py
@@ -0,0 +1,17 @@
+#here for fun, not an assignment.
+
+Hogan2 = "shh secret message"
+SL = "stop snooping through my project"
+import math
+
+x = input ("Hello, What is your name?")
+print ("Hello,", x)
+
+if x == "Coach Mack" or x == "coach mack":
+ print (SL)
+
+if x == "Hogan":
+ print (Hogan2)
+
+if x == int:
+    print (2 + x)
