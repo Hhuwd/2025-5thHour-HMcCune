b9863127ab687639bee2bad43f6138aba54f7584
Hogan McCune <hm127038@student.wpsok.org>
2025-09-02 13:10:32 -0500

    added HW4

---
+++ HW4.py
@@ -0,0 +1,29 @@
+#Name: Hogan McCune
+#Class: 5th Hour
+#Assignment: HW4
+
+
+#1. Print Hello World!
+print ("Hello World!")
+#1. Create a list with 5 strings containing 5 different names in it.
+HoganMultiverse = ["Hoqan", "Hogan", "Thomas", "McCune", "Logan"]
+#2. Append a new name onto the Name List.
+HoganMultiverse.append("AMOGUS")
+#3. Print out the 4th name on the list.
+print (HoganMultiverse[2])
+#4. Create a list with 4 different integers in it.
+h = [10,4,6,2]
+#5. Insert a new integer into the 2nd spot and print the new list.
+h.insert(2,2)
+print (h)
+#6. Sort the list from lowest to highest and print the sorted list.
+h.sort()
+print (h)
+#7. Add the 1st three numbers on the sorted list together and print the sum.
+Hsum = h[0] + h[1] + h[2]
+print (Hsum)
+#8. Create a list with two strings, two variables, and two* boolean values.
+Test = ["1", "2", 6, 3, True, False]
+#9. Create a print statement that asks the user to input their own index value for the list on #8.
+Test.append(input("SODA!!!!!!!"))
+print (Test)
