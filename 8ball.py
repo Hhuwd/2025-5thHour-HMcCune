70de302c1977c8bf55e781f10c80f1e68ad9996c
Hogan McCune <hm127038@student.wpsok.org>
2025-11-12 13:11:07 -0600

    added HW14

--- 8ball.py
+++ 8ball.py
@@ -1,9 +1,14 @@
 import random
-
 eight_ball_responses = ("It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
-                        "You may rely on it","As I see it, yes", "Most likely", "Outlook good",
+                        "You may rely on it","As I see it, yes", "Most likely",
                         "Yes", "Signs point to yes","Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good",
                         "Very doubtful")
-while True:
-    input("What is your question?")
+year = random.randint(2025,3000)
+y = input("Year or Question mode?")
+while y == ("year") or y == "Year":
+    input("What is your Question?")
+    print(year)
+    year = random.randint(2025, 3000)
+while y != ("year") or y == "Year":
+    x = input("What is your question?")
     print(random.choice(eight_ball_responses))
