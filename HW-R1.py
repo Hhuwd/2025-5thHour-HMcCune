173185ba99c972c61300ad6423574e7d1c71814c
Hogan McCune <hm127038@student.wpsok.org>
2026-03-23 13:00:12 -0500

    R1 Added

---
+++ HW-R1.py
@@ -0,0 +1,35 @@
+#Name: Hogan McCune
+#Class: 5th Hour
+#Assignment: HW-R1
+
+#1. Print "Hello World!"
+print("Hello World!")
+#2. Create three variables with strings as values.
+pal = "Hello!"
+bud = "Bye!"
+ohno = "GET OUT!!!"
+#3. Create three variables with booleans as values.
+laptop = True
+phone = False
+macbook = True
+#4. Create three variables with integers as values.
+hogan = 33
+computer = 1230
+o_key = 2
+#5. Create a variable that asks for the user's name and prints it.
+x = input("Please input your name!")
+print(x)
+#6. Add together the three variables from #4 and print the result.
+c = (hogan + computer + o_key)
+print(c)
+#7. Subtract the second variable from the first from #4 and print the result.
+y = (hogan - computer)
+print(y)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+print(o_key * c)
+#9. Create a variable that asks the user for a number with a decimal (a float).
+greg = float(input("GIVE ME A DECIMAL!!!!!"))
+#10. Round the variable from #9 to the nearest whole number and print the result.
+print(round(greg))
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+print(355 % 113)
