1fdba712628a8569cbce6449f87fcf18ba778f94
Hogan McCune <hm127038@student.wpsok.org>
2026-03-25 13:00:07 -0500

    R3 Added

---
+++ HW-R3.py
@@ -0,0 +1,24 @@
+#Name: Hogan McCune
+#Class: 5th Hour
+#Assignment: HW-R3
+
+
+#1. import random and print "Hello World!"
+import random
+print("Hello World!")
+#2. Create three variables that each randomly generate an integer between 1 and 10, print each number on the same line.
+a = random.randint(1,10)
+b = random.randint(1,10)
+c = random.randint(1,10)
+print(a,b,c)
+#3. Create a list containing 5 strings listing 5 colors.
+d = ["Blue","Red","Green","Yellow","Orange"]
+#4. Use a function to randomly choose one of the 5 colors from the list and print the result.
+print(d[random.randint(0,4)])
+#5. Create an if statement that determines which of the three variables from #2 is the lowest.
+if a < b and a < c:
+    print(a, "Is the lowest!")
+elif b < a and b < c:
+    print(b, "Is the lowest!")
+else:
+    print(c, "Is the lowest!")
