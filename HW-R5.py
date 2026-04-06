f653507f24804498f5bcc26dca30afe5dd099fd3
Hogan McCune <hm127038@student.wpsok.org>
2026-04-06 12:56:23 -0500

    R5 finished

---
+++ HW-R5.py
@@ -0,0 +1,14 @@
+#Name: Hogan McCune
+#Class: 5th Hour
+#Assignment: HW-R5
+
+#1. Create a list of the names of all the students in the classroom.
+y = ["Ashton","Bryson","Aiden","Sam","Hogan","Ivan","Dylan","Brennlyn"]
+#2. Create a for loop that prints the names of every student in the list.
+#for i in y:
+    #print(i)
+#3. Using the "in" operator (hint: Google), create a for loop that only prints
+#the name of a student if the letter "e" is in it
+for i in y:
+    if "e" in i:
+        print(i)
